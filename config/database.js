f5bcee972bf6c21da3437b5b31894748402ee754
txt_02 <txt_02@guest-wireless-upc-1607-10-120-112-177.usc.edu>
2017-07-31 15:30:08 -0700

    cred

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     //database: 'mongodb://vy:1234@ds117592.mlab.com:17592/mvp',
-    database: 'mongodb://Allan:Boost@ds149268.mlab.com:49268/programs',
+    database: 'mongodb://Knites:Drift458@ds149268.mlab.com:49268/programs',
     secret: 'yoursecret'
 }
